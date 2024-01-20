f47cc005e1aaae4aaf17038a417ae5c0bc616bde
kristiansnts <epafroditus.kristian@gmail.com>
2024-01-20 21:25:59 +0700

    create season_config

---
+++ includes/config_session.inc.php
@@ -0,0 +1,13 @@
+<?php 
+
+ini_set("session.use_only_cookies", 1);
+ini_set("session.use_strict_mode", 1);
+
+session_set_cookie_params([
+    "lifetime" => 1800,
+    "domain" => "localhost",
+    "path" => "/",
+    "httponly" => true
+]);
+
+session_start();
