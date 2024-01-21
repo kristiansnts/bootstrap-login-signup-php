32c847e4057b13128fbee70a1fcc7130b268ebcb
kristiansnts <epafroditus.kristian@gmail.com>
2024-01-21 12:50:32 +0700

    error handling

--- includes/signup/signup.inc.php
+++ includes/signup/signup.inc.php
@@ -14,7 +14,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         require_once "signup_contr.inc.php";
 
         // Error Handling
-        
+        $errors = [];
+
+        if(is_empty_field($username, $email, $pwd)){
+            $errors["empty_field"] = "Fill all the field!";
+        }
+        if(is_email_invalid($email)){
+            $errors["invalid_email"] = "Input valid email!";
+        }
+        if(is_password_not_match($pwd, $pwdConfirm)){
+            $errors["not_match_password"] = "Password not match";
+        }
+        if(is_username_taken($pdo, $username)){
+            $errors["taken_username"] = "Username already taken";
+        }
+        if(is_email_registered($pdo, $email)){
+            $errors["registered_email"] = "Email already registered";
+        }
+
+        if($errors) {
+            $_SESSION["signup_errors"] = $errors;
+
+            header("Location: ../../index.php");
+            die();
+        }
 
     } catch (PDOException $e) {
         die("Query failed : " . $e->getMessage());
