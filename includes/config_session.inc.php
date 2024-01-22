f6f36d5b01870023b14c67c2fa0a90e99c64287a
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-22 19:39:55 +0700

    create login model & controller

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -13,7 +13,14 @@ session_set_cookie_params([
 session_start();
 
 if(isset($_SESSION["user_id"])){
-
+    if(!isset($_SESSION["last_regenerate"])){
+        regenerate_session_id_loggedIn();
+    } else {
+        $interval = 60 * 30;
+        if (time() - $_SESSION["last_regenerate"] >= $interval){
+            regenerate_session_id_loggedIn();
+        }
+    }
 } else {
     if(!isset($_SESSION["last_regenerate"])){
         regenerate_session_id();
@@ -23,6 +30,17 @@ if(isset($_SESSION["user_id"])){
             regenerate_session_id();
         }
     }
+}
+
+function regenerate_session_id_loggedIn() {
+    session_regenerate_id(true);
+
+    $userId = $_SESSION["user_id"];
+    $newSessionId = session_create_id();
+    $sessionId = $newSessionId . '_' . $userId;
+    session_id($sessionId);
+
+    $_SESSION["last_regenerate"] = time();
 }
 
 function regenerate_session_id() {
--- includes/login/login.inc.php
+++ includes/login/login.inc.php
@@ -27,6 +27,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
             $errors["form-pasword"] = "Username or Password not match!";
         }
         
+        require_once "../config_session.inc.php";
+
+        if($errors) {
+            $_SESSION["login_errors"] = $errors;
+
+            header("Location: ../../index.php");
+            die();
+        }
+
+        $newSessionId = session_create_id();
+        $sessionId = $newSessionId . '_' . $result["id"];
+        session_id($sessionId);
+
+        $_SESSION["user_id"] = $result["id"];
+        $_SESSION["user_username"] = $result["username"];
+
+        $_SESSION["last_regenerate"] = time();
+
+        header("Location: ../../index.php?signin=success");
+        $pdo = null;
+        $stmt = null;
+
+        die();
         
 
     } catch (PDOException $e) {
